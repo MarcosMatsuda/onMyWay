295816da74fa1156f667765bc080fa7b68681eb7
Marcos Matsuda <marcosmatsuda.ti@gmail.com>
2026-03-23 23:14:05 -0300

    chore: upgrade ESLint to v9 flat config with path alias support
    - Create eslint.config.js with FlatCompat for backwards compatibility
    - Configure import resolver for @domain, @data, @infrastructure, @presentation aliases
    - Add eslint-import-resolver-alias for path alias resolution
    - Ignore import/no-unresolved warnings for @ aliases
    - Update package.json dependencies

---
+++ mobile/eslint.config.js
@@ -0,0 +1,40 @@
+const { FlatCompat } = require('@eslint/eslintrc');
+const js = require('@eslint/js');
+const eslintPluginPrettier = require('eslint-plugin-prettier');
+const importPlugin = require('eslint-plugin-import');
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+});
+
+module.exports = [
+  {
+    ignores: ['dist/*', 'node_modules/*', '.expo/*'],
+  },
+  ...compat.extends('expo', 'prettier'),
+  {
+    files: ['src/**/*.{ts,tsx,js,jsx}'],
+    plugins: {
+      prettier: eslintPluginPrettier,
+      import: importPlugin,
+    },
+    settings: {
+      'import/resolver': {
+        alias: {
+          map: [
+            ['@domain', './src/domain'],
+            ['@data', './src/data'],
+            ['@infrastructure', './src/infrastructure'],
+            ['@presentation', './src/presentation'],
+          ],
+          extensions: ['.ts', '.tsx', '.js', '.jsx'],
+        },
+      },
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      'import/no-unresolved': ['error', { ignore: ['^@'] }],
+    },
+  },
+];
